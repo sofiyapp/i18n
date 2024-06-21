45399c9a29dfb9f1bd609c061713a2867c3c9311
Sonya <166842951+sofiyapp@users.noreply.github.com>
2024-06-21 18:05:22 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloI18n.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloI18n
+  },
+  created(){
+    const locale = localStorage.getItem('locale')
+
+    if(locale){
+      this.$i18n.locale = locale;
+    } else if (navigator.language) {
+      //this.$i18n.locale=navigator.language.substring(0,2);
+      this.$i18n.locale=navigator.language;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
